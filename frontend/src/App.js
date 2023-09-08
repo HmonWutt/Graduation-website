8f73e6d5259a365a2fc396b282d73d4f630f2b4b
jocketocke <jcamorling@gmail.com>
2023-09-08 13:34:22 +0200

    Changed the page in App.js
    * Now contains two routes
    * One defautl for any person not logged in
    * The other for a person who has logged in where the form is presented.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Loginpage from "./loginpage";
+import Mainpage from "./Mainpage";
+import Footer from "./Footer";
+import Header from "./Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Loginpage />
-    </div>
+    <Router>
+        <div className="App">
+          <Header/>
+          <Routes>
+              <Route path="/mainpage/:username" element={<Mainpage/>} />
+              <Route path="*" element={ <Loginpage /> } />
+          </Routes>
+          <Footer/>
+        </div>
+    </Router>
   );
 }
 
