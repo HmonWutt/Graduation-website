f7c4b91840ce2eb633c850760b87bd1ecc78fc65
jocketocke <jcamorling@gmail.com>
2023-09-08 15:46:56 +0200

    Had to remove loginPage jsx code because project would not run due to rules of hooks https://react.dev/warnings/invalid-hook-call-warning not being followed

--- frontend/src/loginpage.js
+++ frontend/src/loginpage.js
@@ -2,18 +2,18 @@ import React from "react";
 import { useState, useEffect } from "react";
 import {useNavigate} from "react-router-dom"
 import Logout from "./logout";
-import Button from "react-bootstrap/Button";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Input from "@mui/material/Input";
-import IconButton from "@mui/material/IconButton";
-import InputAdornment from "@mui/material/InputAdornment";
-import Visibility from "@mui/icons-material/Visibility";
-import VisibilityOff from "@mui/icons-material/VisibilityOff";
-import Box from "@mui/material/Box";
+// import Button from "react-bootstrap/Button";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import Input from "@mui/material/Input";
+// import IconButton from "@mui/material/IconButton";
+// import InputAdornment from "@mui/material/InputAdornment";
+// import Visibility from "@mui/icons-material/Visibility";
+// import VisibilityOff from "@mui/icons-material/VisibilityOff";
+// import Box from "@mui/material/Box";
 import axios from "axios";
-import Rocket from "./rocketlauncher.js";
-import ToggleButton from "./toggle";
-import Words from "./words";
+//import Rocket from "./rocketlauncher.js";
+//import ToggleButton from "./toggle";
+//import Words from "./words";
 
 const Loginpage = () => {
   const [username, setUsername] = useState("");
@@ -24,12 +24,12 @@ const Loginpage = () => {
     axios.get("/lala").then((data) => console.log(data));
     return e.target.removeEventListener("click", fetchlala);
   };
-  const handleClickShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault();
-  };
+  //const handleClickShowPassword = () => {
+  //  setShowPassword(!showPassword);
+  //};
+  //const handleMouseDownPassword = (event) => {
+  //  event.preventDefault();
+  //};
   function submit(e) {
     axios
       .post("/login", {
@@ -44,7 +44,7 @@ const Loginpage = () => {
   useEffect(() => {
     axios.get("/testme").then((data) => console.log(data.result));
   }, []);
-  return (
+      return (
     <section
       style={{
         display: "flex",
@@ -53,63 +53,18 @@ const Loginpage = () => {
         gap: "2rem",
       }}
     >
-      <h3 id="welcome" style={{ margin: "2rem", marginTop: "4rem" }}>
-        Welcome to ....
-      </h3>
-      <Words />
-      <ToggleButton />
-      <Box sx={{ display: "flex", flexWrap: "wrap" }}></Box>
-      <div>
-        {" "}
-        <Input
-          sx={{ margin: "0" }}
-          size="small"
-          id="username"
-          variant="outlined"
-          type="text"
-          placeholder="Username"
-          value={password}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div>
-        <Input
-          sx={{ margin: "0" }}
-          size="small"
-          id="password"
-          variant="outlined"
-          type="text"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          endAdornment={
-            <InputAdornment position="end">
-              <IconButton
-                aria-label="toggle password visibility"
-                onClick={handleClickShowPassword}
-                onMouseDown={handleMouseDownPassword}
-                edge="end"
-              >
-                {showPassword ? <VisibilityOff /> : <Visibility />}
-              </IconButton>
-            </InputAdornment>
-          }
-        />
-      </div>
-      <div id="rocket-container">
-        <Rocket />
-      </div>
-      <Button
-        variant="primary"
-        onClick={(e) => {
-          submit(e);
-        }}
-      >
-        Submit
-      </Button>
-      <Button variant="warning" onClick={(e) => fetchlala(e)}>
-        Test
-      </Button>{" "}
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="text"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={submit}>Submit</button>
+      <button onClick={fetchlala}>Test</button>
       <Logout />
     </section>
   );
