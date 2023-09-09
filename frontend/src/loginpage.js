73df46292922349deb257224d96e89775d0c0519
hmon <wu2504hm-s@student.lu.se>
2023-09-09 11:25:59 +0200

    loginpage

--- frontend/src/loginpage.js
+++ frontend/src/loginpage.js
@@ -1,33 +1,34 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import {useNavigate} from "react-router-dom"
-// import Button from "react-bootstrap/Button";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import Input from "@mui/material/Input";
-// import IconButton from "@mui/material/IconButton";
-// import InputAdornment from "@mui/material/InputAdornment";
-// import Visibility from "@mui/icons-material/Visibility";
-// import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { useNavigate } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Input from "@mui/material/Input";
+import IconButton from "@mui/material/IconButton";
+import InputAdornment from "@mui/material/InputAdornment";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 // import Box from "@mui/material/Box";
 import axios from "axios";
-//import Rocket from "./rocketlauncher.js";
+import Rocket from "./rocketlauncher.js";
 //import ToggleButton from "./toggle";
 //import Words from "./words";
 
 const Loginpage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
   const fetchlala = (e) => {
     axios.get("/lala").then((data) => console.log(data));
     return e.target.removeEventListener("click", fetchlala);
   };
-  //const handleClickShowPassword = () => {
-  //  setShowPassword(!showPassword);
-  //};
-  //const handleMouseDownPassword = (event) => {
-  //  event.preventDefault();
-  //};
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
   function submit(e) {
     axios
       .post("/login", {
@@ -35,14 +36,14 @@ const Loginpage = () => {
         password: password,
       })
       .then(function (response) {
-        response.status === 200 && navigate("/mainpage/" + username)
+        response.status === 200 && navigate("/mainpage/" + username);
       })
       .catch((err) => console.log(err));
   }
   useEffect(() => {
     axios.get("/testme").then((data) => console.log(data.result));
   }, []);
-      return (
+  return (
     <section
       style={{
         display: "flex",
@@ -51,18 +52,42 @@ const Loginpage = () => {
         gap: "2rem",
       }}
     >
-      <input
+      <Input
+        sx={{ margin: "0" }}
+        size="small"
+        id="username"
+        variant="outlined"
         type="text"
+        placeholder="Username"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
       />
-      <input
-        type="text"
+
+      <Input
+        sx={{ margin: "0" }}
+        size="small"
+        id="password"
+        variant="outlined"
+        type={showPassword ? "text" : "password"}
+        placeholder="Password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
+        endAdornment={
+          <InputAdornment position="end">
+            <IconButton
+              aria-label="toggle password visibility"
+              onClick={handleClickShowPassword}
+              onMouseDown={handleMouseDownPassword}
+              edge="end"
+            >
+              {showPassword ? <VisibilityOff /> : <Visibility />}
+            </IconButton>
+          </InputAdornment>
+        }
       />
-      <button onClick={submit}>Submit</button>
-      <button onClick={fetchlala}>Test</button>
+      <Rocket />
+      <Button onClick={submit}>Submit</Button>
+      <Button onClick={fetchlala}>Test</Button>
     </section>
   );
 };
