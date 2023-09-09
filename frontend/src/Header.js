4a1e6e2c8d7fe06bbb386ce6e848b9835e9e4cdf
hmon <wu2504hm-s@student.lu.se>
2023-09-09 14:21:07 +0200

    moved axios from logout, added navlink

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,12 +1,24 @@
-import Logout from "./logout";
+import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 const Header = () => {
-    return(
-        <div>
-            <h1> This is a header </h1>
-            <Logout/>
-        </div>
-    );
-}
+  function logout() {
+    axios
+      .get("/logout")
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
+  }
+  return (
+    <NavLink
+      to="/"
+      onClick={logout}
+      className={({ isActive, isPending }) =>
+        isPending ? "pending" : isActive ? "active" : ""
+      }
+    >
+      Log out
+    </NavLink>
+  );
+};
 
 export default Header;
