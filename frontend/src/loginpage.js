212331c976cff65e3ce289e8c6a663d56f9e06a9
hmon <wu2504hm-s@student.lu.se>
2023-09-11 22:34:38 +0200

    added context to pass username and login status

--- frontend/src/loginpage.js
+++ frontend/src/loginpage.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { useState, useEffect, useRef } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect, useRef, createContext } from "react";
+import { useNavigate, Outlet } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Input from "@mui/material/Input";
@@ -11,11 +11,13 @@ import VisibilityOff from "@mui/icons-material/VisibilityOff";
 // import Box from "@mui/material/Box";
 import axios from "axios";
 import Rocket from "./rocketlauncher.js";
-import { useTheme, ThemeProvider, createTheme } from "@mui/material/styles";
 
+export const Context = createContext("");
 const Loginpage = () => {
   const [username, setUsername] = useState("");
+
   const [password, setPassword] = useState("");
+  const [loggedin, setLoggedin] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
   const rocket = useRef();
   const usernameref = useRef();
@@ -36,6 +38,7 @@ const Loginpage = () => {
       .then(function (response) {
         response.status === 200 &&
           setTimeout(() => {
+            setLoggedin(true);
             navigate("/mainpage/" + username);
           }, 1000);
       })
@@ -46,60 +49,66 @@ const Loginpage = () => {
   }, []);
 
   return (
-    <section id="login">
-      <div style={{ marginTop: "5%" }} id="invite">
-        You are cordially invited to my graduation party. Please login to RSVP.
-      </div>
-      <Input
-        ref={usernameref}
-        sx={{ margin: "0" }}
-        style={{ marginTop: "3rem" }}
-        size="small"
-        id="username"
-        variant="outlined"
-        type="text"
-        className="input"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
+    <>
+      {loggedin === false && (
+        <section id="login">
+          <div style={{ marginTop: "5%" }} id="invite">
+            You are cordially invited to my graduation party. Please login to
+            RSVP.
+          </div>
+          <Input
+            ref={usernameref}
+            sx={{ margin: "0" }}
+            style={{ marginTop: "3rem" }}
+            size="small"
+            id="username"
+            variant="outlined"
+            type="text"
+            className="input"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
 
-      <Input
-        ref={passwordref}
-        sx={{ margin: "0" }}
-        className="input "
-        size="small"
-        id="password"
-        variant="outlined"
-        type={showPassword ? "text" : "password"}
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        endAdornment={
-          <InputAdornment position="end">
-            <IconButton
-              aria-label="toggle password visibility"
-              style={{ color: "grey" }}
-              onClick={handleClickShowPassword}
-              onMouseDown={handleMouseDownPassword}
-              edge="end"
-            >
-              {showPassword ? <VisibilityOff /> : <Visibility />}
-            </IconButton>
-          </InputAdornment>
-        }
-      />
-      <Rocket ref={rocket} />
-      <Button
-        id="submit"
-        onClick={() => {
-          submit();
-          rocket.current.launch();
-        }}
-      >
-        Submit
-      </Button>
-    </section>
+          <Input
+            ref={passwordref}
+            sx={{ margin: "0" }}
+            className="input "
+            size="small"
+            id="password"
+            variant="outlined"
+            type={showPassword ? "text" : "password"}
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  style={{ color: "grey" }}
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+          <Rocket ref={rocket} />
+          <Button
+            id="submit"
+            onClick={() => {
+              submit();
+              rocket.current.launch();
+            }}
+          >
+            Submit
+          </Button>
+        </section>
+      )}
+      <Outlet context={(username, loggedin)} />
+    </>
   );
 };
 export default Loginpage;
