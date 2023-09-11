822414cd30e112f3be7a966ef42516a29fef301e
hmon <wu2504hm-s@student.lu.se>
2023-09-11 21:08:03 +0200

    moved toggle button to header

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
 import axios from "axios";
 import Button from "react-bootstrap/esm/Button";
+import ToggleButton from "./toggle";
 
 const Header = () => {
   function logout() {
@@ -10,32 +11,35 @@ const Header = () => {
       .catch((err) => console.log(err));
   }
   return (
-    <NavLink
-      id="nav"
-      to="/"
-      onClick={logout}
-      style={{
-        textDecoration: "none",
-        top: "2%",
-        left: "1%",
-        position: "absolute",
-        fontSize: "0.7rem",
-      }}
-      className={({ isActive, isPending }) =>
-        isPending ? "pending" : isActive ? "active" : ""
-      }
-    >
-      <span
-        className="roundthing"
+    <>
+      <NavLink
+        id="nav"
+        to="/"
+        onClick={logout}
         style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
+          textDecoration: "none",
+          top: "2%",
+          left: "1%",
+          position: "absolute",
+          fontSize: "0.7rem",
         }}
+        className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "active" : ""
+        }
       >
-        Log out
-      </span>
-    </NavLink>
+        <span
+          className="roundthing"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          Log out
+        </span>
+      </NavLink>
+      <ToggleButton />
+    </>
   );
 };
 
