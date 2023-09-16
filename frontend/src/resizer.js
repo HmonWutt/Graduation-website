30a262dde9f6808284974de17e6aed05bdcd9b75
hmon <wu2504hm-s@student.lu.se>
2023-09-16 22:11:15 +0200

    tracks screen resize

---
+++ frontend/src/resizer.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+const useScreenWidth = () => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return screenWidth;
+};
+
+export default useScreenWidth;
